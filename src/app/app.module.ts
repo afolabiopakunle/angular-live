2d1bb6b5595802655c0611c0fd79e83907d66940
afolabiopakunle <opakunleafolabi@gmail.com>
2021-12-03 22:35:01 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { CommonModule } from '@angular/common';  
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';  
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
