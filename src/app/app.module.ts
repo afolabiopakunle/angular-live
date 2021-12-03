352cb1c5eae076b6639c9f3b7c2c80e5586a29de
afolabiopakunle <opakunleafolabi@gmail.com>
2021-12-03 22:09:48 +0100

    pathMatch added for route response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
-    {path: '', component: ProductListComponent}
+    {path: '', component: ProductListComponent},
+    {path: 'products/:productId', component: ProductDetailsComponent, pathMatch: 'full'}
   ]) ],
   declarations: [ AppComponent, ProductListComponent, TopBarComponent, ProductAlertsComponent ],
   bootstrap:    [ AppComponent ]
