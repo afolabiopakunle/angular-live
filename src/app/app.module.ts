14ebd31c2e3d47e2efbd1b3ea82ddd1bc88f3e5e
afolabiopakunle <opakunleafolabi@gmail.com>
2021-12-03 20:28:11 +0100

    getting there

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {path: '', component: ProductsComponent}
+  ]) ],
+  declarations: [ AppComponent, ProductsComponent, TopBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
