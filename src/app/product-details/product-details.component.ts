069c4092ddccba49da373de3a268527df78074d7
afolabiopakunle <opakunleafolabi@gmail.com>
2021-12-04 06:30:06 +0100

    add product to cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 import { Product, products } from '../products';
 
 @Component({
@@ -11,12 +12,18 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, 
+              private cartService: CartService) { }
 
   ngOnInit() {
     const productId = +this.route.snapshot.paramMap.get('productId');
     console.log(productId);
     this.product = products.find(product => product.id === productId)
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Product added to cart!');
   }
 
 }
