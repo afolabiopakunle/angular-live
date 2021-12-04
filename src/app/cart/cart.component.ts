1461f24d7dfa67a8d0564a8812091744addcfd88
afolabiopakunle <opakunleafolabi@gmail.com>
2021-12-05 00:13:35 +0100

    form works and resets

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -14,9 +14,17 @@ export class CartComponent implements OnInit {
 
   constructor(private cartService: CartService,
               private formBuilder: FormBuilder) { }
-
+  checkOutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  })
   ngOnInit() {
     this.items = this.cartService.getItems();
   }
 
+  onSubmit() {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkOutForm.value);
+    this.checkOutForm.reset();
+  }
 }
