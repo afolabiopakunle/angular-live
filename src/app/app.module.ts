134a70e36ac577ed364b7a4e69bb9c3880afb44d
afolabiopakunle <opakunleafolabi@gmail.com>
2021-12-04 06:57:34 +0100

    cart component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+
 import { CartService } from './cart.service';
 
 @NgModule({
@@ -16,7 +18,7 @@ import { CartService } from './cart.service';
     {path: '', component: ProductListComponent},
     {path: 'products/:productId', component: ProductDetailsComponent, pathMatch: 'full'}
   ]) ],
-  declarations: [ AppComponent, ProductListComponent, TopBarComponent, ProductAlertsComponent, ProductDetailsComponent ],
+  declarations: [ AppComponent, ProductListComponent, TopBarComponent, ProductAlertsComponent, ProductDetailsComponent, CartComponent ],
   bootstrap:    [ AppComponent ],
   providers: [CartService]
 })
